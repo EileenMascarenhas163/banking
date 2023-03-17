e3798f54293a2430e48256686d1ad6345194af63
Eileen Mascarenhas <93201259+EileenMascarenhas163@users.noreply.github.com>
2023-03-17 20:59:44 +0530

    Update mybank.sql

--- mybank.sql
+++ mybank.sql
@@ -1,49 +1,62 @@
-create database farzi_bank;
-CREATE TABLE UserSignup (
-uname VARCHAR(30) NOT NULL,
-pswd VARCHAR(30) NOT NULL,
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-        
-create table bank(
-state_branch varchar(30),
-branchno int primary key,
-ifsc varchar(11));
-        
-create table newAccount(
-fname varchar(30),
-lname varchar(30),
-email varchar(30),
-city varchar(30),
-state varchar(30),
-zip int ,
-address varchar(30),
-adhaar varchar(15),
-gender varchar(30),
-dob date,
-phone int,
-accountno VARCHAR(15) primary key,
-t_savings int);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 05, 2022 at 11:13 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE balance(
-    accountno VARCHAR(15),
-    deposit INT,
-    withdraw INT,
-    t_savings INT ,
-    date_ TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-insert into UserSignup values('hnkfjewn','jshikj\cli','2017-12-15 04:30:48');
-insert into bank values('jshikj',5656,'652556252');
-insert into newAccount values('djb\cjdn','jshikj','kdkjdjoc','djchkj','jfaekjh',5625,'jac\kja',
-'556666','ankcan','2017-12-15',66565596,5652,'4556625','56636663',50000);
-insert into balance values('56636663',0,0,50000,current_timestamp());
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `farzi_bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `balance`
+--
+
+CREATE TABLE `balance` (
+  `accountno` varchar(15) DEFAULT NULL,
+  `deposit` int(11) DEFAULT NULL,
+  `withdraw` int(11) DEFAULT NULL,
+  `t_savings` int(11) DEFAULT NULL,
+  `date_` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `balance`
+--
 
 INSERT INTO `balance` (`accountno`, `deposit`, `withdraw`, `t_savings`, `date_`) VALUES
 ('592127632151207', 1000, 0, 14522, '2022-09-05 21:03:17');
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `bank`
+--
+
+CREATE TABLE `bank` (
+  `state_branch` varchar(30) DEFAULT NULL,
+  `branchno` int(11) NOT NULL,
+  `ifsc` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bank`
+--
 
 INSERT INTO `bank` (`state_branch`, `branchno`, `ifsc`) VALUES
 ('Haryana', 7000, '00DB55656'),
@@ -76,21 +89,71 @@ INSERT INTO `bank` (`state_branch`, `branchno`, `ifsc`) VALUES
 ('Assam', 8778, '00DB58778'),
 ('Bihar', 8800, '00DB55656');
 
-     
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newaccount`
+--
+
+CREATE TABLE `newaccount` (
+  `fname` varchar(30) DEFAULT NULL,
+  `lname` varchar(30) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `state` varchar(30) DEFAULT NULL,
+  `zip` int(11) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `adhaar` varchar(15) DEFAULT NULL,
+  `gender` varchar(30) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `accountno` varchar(15) NOT NULL,
+  `t_savings` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `newaccount`
+--
+
 INSERT INTO `newaccount` (`fname`, `lname`, `email`, `city`, `state`, `zip`, `address`, `adhaar`, `gender`, `dob`, `phone`, `accountno`, `t_savings`) VALUES
 ('Eileen', 'Mascarenhas', 'a208.eileenmas@gmail.com', 'Mumbai', 'Andhra Pradesh', 400079, 'C 22/2 BMC colony Parksite Vik', '512551125565315', 'Female', '2022-09-14', 2147483647, '592127632151207', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usersignup`
+--
+
+CREATE TABLE `usersignup` (
+  `uname` varchar(30) NOT NULL,
+  `pswd` varchar(30) NOT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `usersignup`
+--
 
 INSERT INTO `usersignup` (`uname`, `pswd`, `reg_date`) VALUES
 ('leo', '0f759dd1ea6c4c76cedc299039ca4f', '2022-09-05 19:43:37');
 
-UPDATE balance
-SET t_savings = t_savings-40000
-WHERE accountno = '56636663';
+--
+-- Indexes for dumped tables
+--
 
-UPDATE newAccount
-SET t_savings = 10000  
-WHERE accountno = '56636663';   
-select * from UserSignup;
-select * from bank;
-select * from newAccount;
-select * from balance;
+--
+-- Indexes for table `bank`
+--
+ALTER TABLE `bank`
+  ADD PRIMARY KEY (`branchno`);
+
+--
+-- Indexes for table `newaccount`
+--
+ALTER TABLE `newaccount`
+  ADD PRIMARY KEY (`accountno`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
